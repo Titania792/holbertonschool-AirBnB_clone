10af4a5a2a29b051add7d20e6f882ed1cae46300
Tatiana Fernandez <97978908+Titania792@users.noreply.github.com>
2022-06-27 16:46:19 -0300

    BaseModel/Task3 -T

---
+++ models/base_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""class BaseModel that defines all common
+attributes/methods for other classes"""
+
+
+from datetime import datetime
+import uuid
+import json
+
+
+class BaseModel:
+    """"""
+
+    def __init__(self):
+        """Public instance attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Public instance attributes"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates the public instance attribute
+            updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        self.__dict__['__class__'] = self.__class__.__name__
+        return self.__dict__
