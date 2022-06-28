a0a605577a1e126b7a8c074781c8bd74f83cdf01
Tatiana Fernandez <97978908+Titania792@users.noreply.github.com>
2022-06-28 11:55:31 -0300

    adding utc to BaseModel -T

--- models/base_model.py
+++ models/base_model.py
@@ -30,8 +30,8 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """Public instance attributes"""
@@ -40,7 +40,7 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute
             updated_at with the current datetime"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values
